1759fe74249f4d07daefb1294be84aff5c80b1de
Jeet Sadhukhan <jeet.sadhukhan9.2@gmail.com>
2025-11-12 18:47:08 +0530

    Update try_except.py

--- try_except.py
+++ try_except.py
@@ -3,7 +3,20 @@ try:
     result = 10 / num
 #If you give zero as input then it will give ZeroDivisionError
 #If you give string then it will give value error
-except ZeroDivisionError:
+except:
     print("You can't divide by zero!")
-except ValueError:
-    print("Invalid input! Please enter a number.")
+# except ZeroDivisionError:
+#     print("You can't divide by zero!")
+# except ValueError:
+#     print("Invalid input! Please enter a number.")
+
+try:
+    num = int(input("Enter a number: "))
+    print(num)
+except Exception as e:
+    print("Some error occurred!", e)
+
+a=int(input("Enter a numbe 1:"))
+b=int(input("Enter a numbe 2:"))
+if b==0:
+    raise ValueError("Please don't divide 0")
